2a9ef6b0d21a0d201b79a462f3317e8904768a65
MoiraDust <moiradust@foxmail.com>
2022-04-21 16:51:26 +1000

    login page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import { Button } from "antd";
 
 export default function App() {
   return (
     <>
       app
-      <Button type="primary">Primary Button</Button>
       <Outlet />
     </>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,61 @@
 import React from "react";
+import { Form, Input, Button, Checkbox } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import "./less/Login.less";
+import LogoImg from "../assets/logo.jpeg";
 
 export default function Login() {
-  return <div>Login</div>;
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+  return (
+    <div className="login">
+      <div className="login_box">
+        <img src={LogoImg} alt="no image" />
+        <Form
+          name="basic"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: "Please input your username!",
+              },
+            ]}
+          >
+            <Input placeholder="User Name" prefix={<UserOutlined />} />
+          </Form.Item>
+
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+            ]}
+          >
+            <Input.Password placeholder="Password" prefix={<LockOutlined />} />
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" block>
+              Login
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
 }
